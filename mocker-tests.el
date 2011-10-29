08029db8a21273fcd590afe1af87c43659afc957
Yann Hodique <yann.hodique@gmail.com>
2011-10-29 09:18:59 +0200

    move tests to their own module

---
+++ mocker-tests.el
@@ -0,0 +1,122 @@
+;;; mocker-tests.el --- tests for mocker.el
+
+;; Copyright (C) 2011  Free Software Foundation, Inc.
+
+;; Author: Yann Hodique <yann.hodique@gmail.com>
+;; Keywords: lisp, testing
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This file contains tests for mocker.el
+
+;;; Code:
+
+(require 'mocker)
+
+(eval-when-compile
+  (when (null (ignore-errors (require 'ert)))
+    (defmacro* ert-deftest (name () &body docstring-keys-and-body)
+      (message "Skipping tests, ERT is not available"))))
+
+(ert-deftest mocker-let-basic ()
+  (should
+   (eq t (mocker-let () t))))
+
+(ert-deftest mocker-let-single ()
+  (should
+   (eq t
+       (mocker-let ((foo () :records ((:output t))))
+         (foo)))))
+
+(ert-deftest mocker-let-no-record ()
+  (should-error
+   (mocker-let ((foo () :records ()))
+     (foo))
+   :type 'mocker-mock-error))
+
+(ert-deftest mocker-let-multiple ()
+  (should
+   (eq 42
+       (mocker-let ((foo () :records ((:output 6)))
+                    (bar () :records ((:output 7))))
+         (* (foo) (bar))))))
+
+(ert-deftest mocker-let-nested ()
+  (should
+   (eq 42
+       (mocker-let ((foo () :records ((:output 6))))
+         (mocker-let ((bar () :records ((:output 7))))
+           (* (foo) (bar)))))))
+
+(ert-deftest mocker-let-multiple-inputs ()
+  (should
+   (eq 42
+       (mocker-let ((foo (x) :records ((:input (1) :output 6)))
+                    (bar (x) :records ((:input (2) :output 7))))
+         (* (foo 1) (bar 2))))))
+
+(ert-deftest mocker-let-multiple-inputs-invalid ()
+  (should-error
+   (mocker-let ((foo (x) :records ((:input (1) :output 6)))
+                (bar (x) :records ((:input (2) :output 7))))
+     (* (foo 2) (bar 2)))
+   :type 'mocker-record-error))
+
+(ert-deftest mocker-let-single-input-matcher ()
+  (should
+   (eq t
+       (mocker-let ((foo (x)
+                         :records ((:input-matcher (lambda (x) (evenp x))
+                                                   :output t))))
+         (foo 4)))))
+
+(ert-deftest mocker-let-single-input-matcher-invalid ()
+  (should-error
+   (mocker-let ((foo (x)
+                     :records ((:input-matcher (lambda (x) (evenp x))
+                                               :output t))))
+     (foo 5))
+   :type 'mocker-record-error))
+
+(ert-deftest mocker-let-multiple-output-generator ()
+  (should
+   (eq 2
+       (mocker-let ((foo (x)
+                         :records ((:input (2) :output-generator identity)
+                                   (:input (4) :output-generator
+                                           (lambda (x) 0)))))
+         (+ (foo 2) (foo 4))))))
+
+(ert-deftest mocker-let-multiple-calls-min ()
+  (should
+   (eq 4
+       (mocker-let ((foo (x)
+                         :records ((:input (2) :output-generator identity
+                                           :min-occur 2))))
+         (+ (foo 2) (foo 2))))))
+
+(ert-deftest mocker-let-multiple-calls-illimited ()
+  (should
+   (eq 8
+       (mocker-let ((foo (x)
+                         :records ((:input (2) :output-generator identity
+                                           :max-occur 4))))
+         (+ (foo 2) (foo 2) (foo 2) (foo 2))))))
+
+(provide 'mocker-tests)
+;;; mocker-tests.el ends here
