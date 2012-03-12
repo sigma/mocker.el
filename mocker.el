7f9cd9f8919ffacf2fad248befb992d1276abb05
Yann Hodique <yann.hodique@gmail.com>
2012-03-12 20:12:09 +0100

    bump version to 0.2

--- mocker.el
+++ mocker.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords: lisp, testing
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((eieio "1.3"))
 
 ;; This file is free software; you can redistribute it and/or modify
